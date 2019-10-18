e25fcf1c38b457c535d04b4a47dea94b53ce0d09
Svetlana Peksheva (DevExpress) <nikishina@corp.devexpress.com>
2019-10-18 18:35:42 +0300

    show flyout hints

---
+++ CS/ToolTipControllerExample/Main.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.XtraTreeList;
+using DevExpress.XtraEditors;
+using DevExpress.Utils;
+
+namespace ToolTipControllerExample {
+    public partial class Main : XtraForm {
+        DetailControl flyoutControl;
+        public Main() {
+            InitializeComponent();
+            treeList1.DataSource = DataHelper.GetData(10);
+        }
+        DetailControl FlyoutControl {
+            get {
+                if(flyoutControl == null)
+                    flyoutControl = new DetailControl();
+                return flyoutControl;
+            }
+        }
+        void OnGetActiveObjectInfo(object sender, ToolTipControllerGetActiveObjectInfoEventArgs e) {
+            TreeList tree = e.SelectedControl as TreeList;
+            if(tree != null) {
+                TreeListHitInfo hitInfo = tree.CalcHitInfo(e.ControlMousePosition);
+                Record record = (Record)tree.GetDataRecordByNode(hitInfo.Node);
+                if(hitInfo.HitInfoType == HitInfoType.Cell) {
+                    e.Info = new ToolTipControlInfo();
+                    e.Info.Object = $"Column={hitInfo.Column.FieldName}, NodeId={hitInfo.Node.Id}";
+                    e.Info.FlyoutControl = FlyoutControl;
+                    FlyoutControl.UpdateContent(record);
+                }
+            }
+        }
+    }
+}
--- CS/ToolTipControllerExample/Program.cs
+++ CS/ToolTipControllerExample/Program.cs
@@ -12,7 +12,7 @@ namespace ToolTipControllerExample {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
